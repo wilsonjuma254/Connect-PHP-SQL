7cf4b5b4d9008c09fa6ac92204cf8000e8cfbd51
Wilsonmobisa <willexjumah12338@gmail.com>
2024-08-15 18:10:27 +0300

    insert data from a website into a database

---
+++ 2.insert-data-from-web-to-db.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Connect PHP SQL</title>
+</head>
+
+<!--using form-->
+<form action="includes/2.php" method="POST">
+    <input type="text" name="first" placeholder="First Name" id="">
+    <br>
+    <input type="text" name="last" placeholder="Last Name" id="">
+    <br>
+    <input type="text" name="email" placeholder="E-Mail" id="">
+    <br>
+    <input type="text" name="first" placeholder="First Name" id="">
+    <br>
+    <input type="text" name="uid" placeholder="Username" id="">
+    <br>
+    <input type="password" name="pwd" placeholder="Password" id="">
+    <br>
+    <button type="submit" name="submit">Sign Up</button>
+
+</form>
+
+
+<?php
+
+
+
+
+
+?>
+
+</body>
+</html>
